fc61aff28e6f3f7c416053c22a26e1a457c093b4
peeech <pj@imagine-nyc.com>
2020-12-11 15:26:59 +0100

    correct name of bin

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use anyhow::{Context, Result};
 
-use self_hosted_happs::{install_happs, load_happs_yaml, Config};
+use configure_holochain::{install_happs, load_happs_yaml, Config};
 use tracing::instrument;
 use tracing_subscriber::EnvFilter;
 
