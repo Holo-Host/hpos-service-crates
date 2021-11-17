1f77ba9d0a0857a77442ac8b8b1e2857cd730a5b
timotree3 <timorcb@gmail.com>
2021-11-17 14:20:50 -0500

    Add option to disable servicelogger

--- src/config.rs
+++ src/config.rs
@@ -19,6 +19,9 @@ pub struct Config {
     pub happs_file_path: PathBuf,
     /// Path to a YAML file containing hApp membrane proofs
     pub membrane_proofs_file_path: PathBuf,
+    #[structopt(long, env)]
+    /// If passed, causes existing existing happs that have `::servicelogger` to be disabled
+    pub disable_servicelogger: bool,
 }
 
 impl Config {
