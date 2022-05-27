3295e8e6b3eef479632dcf9ff4fbdcd001c8802c
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2022-05-27 10:57:39 +0100

    added use failure::*;

--- src/websocket.rs
+++ src/websocket.rs
@@ -16,6 +16,7 @@ use hpos_config_core::Config;
 use std::{collections::HashMap, env, fs, fs::File, io::prelude::*, sync::Arc};
 use tracing::{info, instrument, trace};
 use url::Url;
+use failure::*;
 
 use crate::config::Happ;
 
@@ -135,13 +136,7 @@ impl AdminWebsocket {
         let holochain_public_key =
             hpos_config_seed_bundle_explorer::holoport_public_key(&config, password).await?;
         // Get mem-proof by registering on the ops-console
-        if let Err(e) = self
-            .try_registration_auth(&config, holochain_public_key)
-            .await
-        {
-            error!("{}", e);
-            return Err(e);
-        }
+        self.try_registration_auth(&config, holochain_public_key).await
     }
 
     #[instrument(skip(self), err)]
