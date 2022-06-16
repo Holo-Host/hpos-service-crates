1d4e7f083f1e176c08ae4a3c310d58d64ecafab0
zo-el <joelulahanna@gmail.com>
2022-06-16 19:54:50 -0400

    configure-holochain: handle dual DNA for core happs

---
+++ src/utils.rs
@@ -0,0 +1,103 @@
+use anyhow::{anyhow, Context, Result};
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process;
+use tempfile::TempDir;
+use tracing::{debug, instrument};
+use url::Url;
+
+pub type HappIds = Vec<String>;
+
+#[instrument(
+    err,
+    fields(
+        source_path = %source_path.as_ref().display(),
+        unpack_path = %unpack_path.as_ref().display(),
+    ),
+)]
+pub(crate) fn extract_zip<P: AsRef<Path>>(source_path: P, unpack_path: P) -> Result<()> {
+    let _ = fs::remove_dir_all(unpack_path.as_ref());
+    fs::create_dir(unpack_path.as_ref()).context("failed to create empty unpack_path")?;
+
+    debug!("unziping file");
+
+    let output = process::Command::new("unzip")
+        .arg(source_path.as_ref().as_os_str())
+        .arg("-d")
+        .arg(unpack_path.as_ref().as_os_str())
+        .stdout(process::Stdio::piped())
+        .output()
+        .context("failed to spawn unzip command")?;
+
+    debug!("{}", String::from_utf8_lossy(&output.stdout));
+
+    Ok(())
+}
+
+#[instrument(err, skip(url))]
+pub(crate) async fn download_file(url: &Url) -> Result<PathBuf> {
+    use isahc::config::RedirectPolicy;
+    use isahc::prelude::*;
+
+    let path = if url.scheme() == "file" {
+        let p = PathBuf::from(url.path());
+        debug!("Using: {:?}", p);
+        p
+    } else {
+        debug!("downloading");
+        let mut url = Url::clone(url);
+        url.set_scheme("https")
+            .map_err(|_| anyhow!("failed to set scheme to https"))?;
+        let client = HttpClient::builder()
+            .redirect_policy(RedirectPolicy::Follow)
+            .build()
+            .context("failed to initiate download request")?;
+        let mut response = client
+            .get(url.as_str())
+            .context("failed to send GET request")?;
+        if !response.status().is_success() {
+            return Err(anyhow!(
+                "response status code {} indicated failure",
+                response.status().as_str()
+            ));
+        }
+        let dir = TempDir::new().context("failed to create tempdir")?;
+        let url_path = PathBuf::from(url.path());
+        let basename = url_path
+            .file_name()
+            .context("failed to get basename from url")?;
+        let path = dir.into_path().join(basename);
+        let mut file = fs::File::create(&path).context("failed to create target file")?;
+        response
+            .copy_to(&mut file)
+            .context("failed to write response to file")?;
+        debug!("download successful");
+        path
+    };
+    Ok(path)
+}
+
+// Returns true if app should be kept active in holochain
+pub fn keep_app_active(installed_app_id: &str, happs_to_keep: HappIds) -> bool {
+    happs_to_keep.contains(&installed_app_id.to_string()) || installed_app_id.contains("uhCkk")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_keep_app_active() {
+        let happs_to_keep = vec!["elemental-chat:2".to_string(), "hha:1".to_string()];
+        let app_1 = "elemental-chat:1";
+        let app_2 = "elemental-chat:2";
+        let app_3 = "uhCkkcF0X1dpwHFeIPI6-7rzM6ma9IgyiqD-othxgENSkL1So1Slt::servicelogger";
+        let app_4 = "other-app";
+
+        assert_eq!(keep_app_active(app_1, happs_to_keep.clone()), false);
+        assert_eq!(keep_app_active(app_2, happs_to_keep.clone()), true); // because it is in config
+        assert_eq!(keep_app_active(app_3, happs_to_keep.clone()), true); // because it is hosted
+        assert_eq!(keep_app_active(app_4, happs_to_keep.clone()), false);
+    }
+}
