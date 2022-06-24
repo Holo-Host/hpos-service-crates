33f154132556d0b9117631400796e629c6f80107
peeech <pj@imagine-nyc.com>
2022-06-24 06:46:29 +0200

    update odds and ends to new versions for CI

--- holochain_version.nix
+++ holochain_version.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --git-src=revision:holochain-0.0.126 --lair-version-req=0.1.0 --output-file=holochain_version.nix
+# update-holochain-versions --git-src=revision:holochain-0.0.145 --lair-version-req=~0.1 --output-file=holochain_version.nix
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "holochain-0.0.131";
-    sha256 = "sha256-zEsVDzbhMBBp6hJ4d79PcVIjFTl+Lmpwkl5TAmqZOyM=";
+    rev = "holochain-0.0.145";
+    sha256 = "sha256-NyhR+Sa7B/pi7Wou9A0EoRndoChTJudjhmaBm5zN86I=";
     cargoLock = {
         outputHashes = {
         };
@@ -15,19 +15,21 @@
         "holochain"
         "hc"
         "kitsune-p2p-proxy"
+        "kitsune-p2p-tx2-proxy"
     ];
 
     rustVersion = "1.61.0";
 
     lair = {
         url = "https://github.com/holochain/lair";
-        rev = "v0.0.9";
-        sha256 = "sha256-glSixh2GtWtJ1wswAA0Q2hnLIFPQY+Tsh36IcUgIbRs=";
+        rev = "lair_keystore_api-v0.1.3";
+        sha256 = "sha256-1amhBe34dEOlTATryHdKaz/NMUk2Mnn79VrahvO4OnY=";
 
         binsFilter = [
             "lair-keystore"
         ];
 
+        rustVersion = "1.61.0";
 
         cargoLock = {
             outputHashes = {
