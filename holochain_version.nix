03e0781f63e5ae86ae75eeea6c493633e631db80
zo-el <joelulahanna@gmail.com>
2022-10-03 15:29:19 -0500

    holochain: v0.0.165

--- holochain_version.nix
+++ holochain_version.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --git-src=revision:holochain-0.0.160 --output-file=holochain_version.nix
+# update-holochain-versions --git-src=revision:holochain-0.0.165 --output-file=holochain_version.nix
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "holochain-0.0.160";
-    sha256 = "sha256-B0xYbik2e08fr8OhxpIaQ0vlPROi+FdwRm+rldpSadk=";
+    rev = "holochain-0.0.165";
+    sha256 = "sha256-rjRdYLYhO8nrigCIK76eGcXOqxNZDkhrslXeiYSCZNk=";
     cargoLock = {
         outputHashes = {
         };
@@ -18,17 +18,15 @@
         "kitsune-p2p-tx2-proxy"
     ];
 
-
     lair = {
         url = "https://github.com/holochain/lair";
-        rev = "lair_keystore_api-v0.2.0";
-        sha256 = "sha256-n7nZyZR0Q68Uff7bTSVFtSDLi21CNcyKibOBx55Gasg=";
+        rev = "lair_keystore_api-v0.2.1";
+        sha256 = "sha256-ty5gGI9XIZJwV/kZ9DUpjbR2oneJpJVsmYgHLEnV+18=";
 
         binsFilter = [
             "lair-keystore"
         ];
-
-
+        
         cargoLock = {
             outputHashes = {
             };
