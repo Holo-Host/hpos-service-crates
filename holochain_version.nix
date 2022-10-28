3563c9ee1388074a422faa811e1ffae9ffdcc140
zo-el <joelulahanna@gmail.com>
2022-10-27 21:52:06 -0500

    holochain_conductor_api: v0.0.66

--- holochain_version.nix
+++ holochain_version.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --git-src=revision:holochain-0.0.165 --output-file=holochain_version.nix
+# update-holochain-versions --git-src=revision:holochain-0.0.169 --output-file=holochain_version.nix
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "holochain-0.0.165";
-    sha256 = "sha256-rjRdYLYhO8nrigCIK76eGcXOqxNZDkhrslXeiYSCZNk=";
+    rev = "holochain-0.0.169";
+    sha256 = "sha256-O5ieU54YH3M3Rl/0XT9YKomn1NRDTmypB3Nq2WuIjAA=";
     cargoLock = {
         outputHashes = {
         };
@@ -18,6 +18,7 @@
         "kitsune-p2p-tx2-proxy"
     ];
 
+
     lair = {
         url = "https://github.com/holochain/lair";
         rev = "lair_keystore_api-v0.2.1";
@@ -26,7 +27,8 @@
         binsFilter = [
             "lair-keystore"
         ];
-        
+
+
         cargoLock = {
             outputHashes = {
             };
