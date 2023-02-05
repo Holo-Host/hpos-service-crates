29507fc72a3ac92dd29b19922b44380675852b4e
zo-el <joelulahanna@gmail.com>
2023-02-05 16:33:48 -0600

    holochain: v0.1.0

--- holochain_version.nix
+++ holochain_version.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --git-src=revision:holochain-0.1.0-beta-rc.4 --output-file=holochain_version.nix
+# update-holochain-versions --git-src=revision:holochain-0.1.0 --output-file=holochain_version.nix
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "holochain-0.1.0-beta-rc.4";
-    sha256 = "sha256-Rr66+kZf5GTnXlhyBfM3U0uXJU2k3l4xSMcH23x0Wz4=";
+    rev = "holochain-0.1.0";
+    sha256 = "sha256-jSEWpZut7OgdBVPOpLoLPGAZT0+pnSngvc5oEsNy67M=";
     cargoLock = {
         outputHashes = {
         };
