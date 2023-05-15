82a4cbb3729684eeb9d51cde4b1a5e9261956f5c
zo-el <joelulahanna@gmail.com>
2023-05-15 14:41:12 -0500

    holochain v0.2.0

--- flake.nix
+++ flake.nix
@@ -1,40 +1,41 @@
 {
   description = "Template for Holochain app development";
 
-  inputs = {
-    nixpkgs.follows = "holochain-flake/nixpkgs";
-
     # this can now be udpated directly, e.g.:
     # nix flake lock --override-input holochain github:holochain/holochain/holochain-0.1.3
-    holochain.url = "github:holochain/holochain";
-    holochain.flake = false;
 
-    holochain-flake = {
-      url = "github:holochain/holochain";
-      inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
-      inputs.holochain.url = "github:holochain/holochain/holochain-0.1.3";
-      # inputs.holochain.follows = "holochain";
-    };
+  inputs = {
+    versions.url = "github:holochain/holochain?dir=versions/0_1";
+    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.0";
+    versions.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.2.4";
+
+    holochain-flake.url = "github:holochain/holochain";
+    holochain-flake.inputs.versions.follows = "versions";
+
+    nixpkgs.follows = "holochain-flake/nixpkgs";
+    flake-parts.follows = "holochain-flake/flake-parts";
+     # inputs.holochain.follows = "holochain";
   };
 
-  outputs = inputs @ { ... }:
-    inputs.holochain-flake.inputs.flake-parts.lib.mkFlake
-      {
-        inherit inputs;
-      }
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; }
       {
         systems = builtins.attrNames inputs.holochain-flake.devShells;
+
         perSystem =
-          { config
+          { inputs'
+          , config
           , pkgs
           , system
           , ...
           }: {
+
             devShells.default = pkgs.mkShell {
-              inputsFrom = [ inputs.holochain-flake.devShells.${system}.holonix ];
-              packages = with pkgs; [
-                  # more packages go here
-                  pkgs.nodejs-18_x pkgs.binaryen 
+              inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
+              packages = [
+                pkgs.nodejs-18_x
+                pkgs.binaryen
+                # more packages go here
               ];
             };
           };
