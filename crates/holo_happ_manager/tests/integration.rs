c12779bc42efbfc1ed8f9c1c62a3a4806af9f0d0
zo-el <joelulahanna@gmail.com>
2023-06-13 20:50:39 -0500

    holo_happ_manage: integration test fix

--- crates/holo_happ_manager/tests/integration.rs
+++ crates/holo_happ_manager/tests/integration.rs
@@ -8,8 +8,59 @@ async fn run_happ_manager() {
     use std::env::set_var;
     use std::path::PathBuf;
 
-    let config = setup_test();
+    // Point HPOS_CONFIG_PATH to test config file
+    set_var(
+        "HPOS_CONFIG_PATH",
+        "../test_utils/config/hp-primary-bzywj.json",
+    );
 
+    let tmp_dir = test_utils::holochain::create_tmp_dir();
+    let log_dir = test_utils::holochain::create_log_dir();
+
+    // Set HOST_PUBKEY_PATH in a writable temp location
+    set_var("HOST_PUBKEY_PATH", &tmp_dir.clone().join("agent.key"));
+
+    // Set MEM_PROOF_PATH in a writable temp location
+    set_var("MEM_PROOF_PATH", &tmp_dir.join("mem-proof"));
+
+    // Set HOLOFUEL_INSTANCE_ROLE for the mem-proof server
+    set_var("HOLOFUEL_INSTANCE_ROLE", "host");
+
+    // On devNet holoports force random key
+    set_var("FORCE_RANDOM_AGENT_KEY", "1");
+
+    // Holoports do not force read-only memproof
+    set_var("READ_ONLY_MEM_PROOF", "false");
+
+    // devNet HBS server url, because given hpos-config is registered in devNet database
+    set_var("MEM_PROOF_SERVER_URL", "https://hbs.dev.holotest.net");
+
+    // pass to unlock the seed
+    set_var("DEVICE_SEED_DEFAULT_PASSWORD", "passphrase");
+
+    let device_bundle = match get_hpos_config().unwrap() {
+        Config::V2 { device_bundle, .. } => device_bundle,
+        _ => panic!("Unsupported Config version"),
+    };
+
+    // spin up lair
+    println!("Starting lair-keystore");
+    let (_lair, lair_config) =
+        test_utils::lair::spawn(&tmp_dir, &log_dir, &device_bundle, None).unwrap();
+
+    println!("Spinning up holochain");
+    let _holochain =
+        test_utils::holochain::spawn_holochain(&tmp_dir, &log_dir, lair_config.clone());
+
+    let happs_file_path: PathBuf = "./tests/config.yaml".into();
+    let config = hpos_hc_connect::holo_config::Config {
+        admin_port: 4444,
+        happ_port: 42233,
+        ui_store_folder: "./tmp".into(),
+        happs_file_path: happs_file_path.clone(),
+        lair_url: Some(lair_config.connection_url.to_string()),
+    };
+    println!("Test running with config: {:?}", &config);
     println!("Run configure holochain script to install HHA");
     configure_holochain::run(config.clone()).await.unwrap();
 
@@ -31,61 +82,4 @@ async fn run_happ_manager() {
     assert_eq!(published_happ[0].bundle_url, "https://");
     assert_eq!(published_happ[1].bundle_url, "https://");
     println!("Successfully tested!");
-
-    fn setup_test() -> hpos_hc_connect::holo_config::Config {
-        // Point HPOS_CONFIG_PATH to test config file
-        set_var(
-            "HPOS_CONFIG_PATH",
-            "../test_utils/config/hp-primary-bzywj.json",
-        );
-
-        let tmp_dir = test_utils::holochain::create_tmp_dir();
-        let log_dir = test_utils::holochain::create_log_dir();
-
-        // Set HOST_PUBKEY_PATH in a writable temp location
-        set_var("HOST_PUBKEY_PATH", &tmp_dir.clone().join("agent.key"));
-
-        // Set MEM_PROOF_PATH in a writable temp location
-        set_var("MEM_PROOF_PATH", &tmp_dir.join("mem-proof"));
-
-        // Set HOLOFUEL_INSTANCE_ROLE for the mem-proof server
-        set_var("HOLOFUEL_INSTANCE_ROLE", "host");
-
-        // On devNet holoports force random key
-        set_var("FORCE_RANDOM_AGENT_KEY", "1");
-
-        // Holoports do not force read-only memproof
-        set_var("READ_ONLY_MEM_PROOF", "false");
-
-        // devNet HBS server url, because given hpos-config is registered in devNet database
-        set_var("MEM_PROOF_SERVER_URL", "https://hbs.dev.holotest.net");
-
-        // pass to unlock the seed
-        set_var("DEVICE_SEED_DEFAULT_PASSWORD", "passphrase");
-
-        let device_bundle = match get_hpos_config().unwrap() {
-            Config::V2 { device_bundle, .. } => device_bundle,
-            _ => panic!("Unsupported Config version"),
-        };
-
-        // spin up lair
-        println!("Starting lair-keystore");
-        let (_lair, lair_config) =
-            test_utils::lair::spawn(&tmp_dir, &log_dir, &device_bundle, None).unwrap();
-
-        println!("Spinning up holochain");
-        let _holochain =
-            test_utils::holochain::spawn_holochain(&tmp_dir, &log_dir, lair_config.clone());
-
-        let happs_file_path: PathBuf = "./tests/config.yaml".into();
-        let config = hpos_hc_connect::holo_config::Config {
-            admin_port: 4444,
-            happ_port: 42233,
-            ui_store_folder: "./tmp".into(),
-            happs_file_path: happs_file_path.clone(),
-            lair_url: Some(lair_config.connection_url.to_string()),
-        };
-        println!("Test running with config: {:?}", &config);
-        return config;
-    }
 }
