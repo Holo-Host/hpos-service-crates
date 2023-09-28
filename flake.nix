2d6eb0614d6806610456fb0ce5b52176050059e5
zo-el <joelulahanna@gmail.com>
2023-09-28 13:59:51 -0500

    flake update

--- flake.nix
+++ flake.nix
@@ -1,20 +1,14 @@
 {
   description = "Template for Holochain app development";
 
-    # this can now be udpated directly, e.g.:
-    # nix flake lock --override-input holochain github:holochain/holochain/holochain-0.1.3
-
   inputs = {
     versions.url = "github:holochain/holochain?dir=versions/0_2";
-    versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.2-beta-rc.1";
-    versions.inputs.lair.url = "github:holochain/lair/lair_keystore-v0.3.0";
  
     holochain-flake.url = "github:holochain/holochain";
     holochain-flake.inputs.versions.follows = "versions";
 
     nixpkgs.follows = "holochain-flake/nixpkgs";
     flake-parts.follows = "holochain-flake/flake-parts";
-     # inputs.holochain.follows = "holochain";
   };
 
   outputs = inputs:
