5df1961f89430ff80fc2e7e1eace0c5f0c39fc03
peeech <pj@imagine-nyc.com>
2024-05-20 20:56:26 +0200

    add zome_call_typed method to app_ws

--- crates/hpos_connect_hc/src/app_ws.rs
+++ crates/hpos_connect_hc/src/app_ws.rs
@@ -1,9 +1,13 @@
-use crate::utils::WsPollRecv;
+use crate::utils::{fresh_nonce, WsPollRecv};
 use anyhow::{anyhow, Context, Result};
 use holochain_conductor_api::{
-    AppAuthenticationRequest, AppAuthenticationToken, AppInfo, AppRequest, AppResponse, ZomeCall,
+    AppAuthenticationRequest, AppAuthenticationToken, AppInfo, AppRequest, AppResponse,
+    ProvisionedCell, ZomeCall,
 };
+use holochain_keystore::MetaLairClient;
+use holochain_types::prelude::{ExternIO, FunctionName, ZomeCallUnsigned, ZomeName};
 use holochain_websocket::{connect, ConnectRequest, WebsocketConfig, WebsocketSender};
+use serde::{de::DeserializeOwned, Serialize};
 use std::{net::ToSocketAddrs, sync::Arc};
 use tracing::{instrument, trace};
 
@@ -52,6 +56,43 @@ impl AppWebsocket {
     pub async fn zome_call(&mut self, msg: ZomeCall) -> Result<AppResponse> {
         let app_request = AppRequest::CallZome(Box::new(msg));
         self.send(app_request).await
+    }
+
+    pub async fn zome_call_typed<T, R>(
+        &mut self,
+        keystore: MetaLairClient,
+        cell: ProvisionedCell,
+        zome_name: ZomeName,
+        fn_name: FunctionName,
+        payload: T,
+    ) -> Result<R>
+    where
+        T: Serialize + std::fmt::Debug,
+        R: DeserializeOwned,
+    {
+        let (nonce, expires_at) = fresh_nonce()?;
+        let zome_call_unsigned = ZomeCallUnsigned {
+            cell_id: cell.cell_id.clone(),
+            zome_name,
+            fn_name,
+            payload: ExternIO::encode(payload)?,
+            cap_secret: None,
+            provenance: cell.cell_id.agent_pubkey().clone(),
+            nonce,
+            expires_at,
+        };
+        let signed_zome_call =
+            ZomeCall::try_from_unsigned_zome_call(&keystore, zome_call_unsigned).await?;
+
+        let response = self.zome_call(signed_zome_call).await?;
+
+        match response {
+            AppResponse::ZomeCalled(r) => {
+                let response: R = rmp_serde::from_slice(r.as_bytes())?;
+                Ok(response)
+            }
+            _ => Err(anyhow!("unexpected ZomeCallresponse: {:?}", response)),
+        }
     }
 
     #[instrument(skip(self))]
