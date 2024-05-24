b25d8faae34e5f49380be84f706a25cc9d2720a0
peeech <pj@imagine-nyc.com>
2024-05-24 11:25:16 +0200

    update core-app-cli

--- crates/core_app_cli/src/actions/pay_invoices.rs
+++ crates/core_app_cli/src/actions/pay_invoices.rs
@@ -1,24 +1,25 @@
 use anyhow::Result;
 use holochain_types::prelude::Timestamp;
 use holochain_types::prelude::{
-    holochain_serial, EntryHashB64, ExternIO, FunctionName, SerializedBytes, ZomeName,
+    holochain_serial, EntryHashB64, FunctionName, SerializedBytes, ZomeName,
 };
+use hpos_hc_connect::app_connection::CoreAppRoleName;
+use hpos_hc_connect::hha::HHAAgent;
 use hpos_hc_connect::holofuel_types::Pending;
-use hpos_hc_connect::{CoreAppAgent, CoreAppRoleName};
 use serde::{Deserialize, Serialize};
 
 pub async fn get() -> Result<()> {
-    let mut agent = CoreAppAgent::connect().await?;
-    let result = agent
-        .zome_call(
-            CoreAppRoleName::Holofuel,
+    let mut agent = HHAAgent::spawn(None).await?;
+
+    let txs: Pending = agent
+        .app
+        .zome_call_typed(
+            CoreAppRoleName::Holofuel.into(),
             ZomeName::from("transactor"),
             FunctionName::from("get_pending_transactions"),
-            ExternIO::encode(())?,
+            (),
         )
         .await?;
-
-    let txs: Pending = rmp_serde::from_slice(result.as_bytes())?;
 
     #[derive(Serialize, Deserialize, Debug, SerializedBytes)]
     struct AcceptTx {
@@ -30,26 +31,30 @@ pub async fn get() -> Result<()> {
         println!("Going to accept first transaction");
         println!("Invoices Pending: {:?}", txs.invoice_pending[0]);
         println!("===================");
-        let result = agent
-            .zome_call(
-                CoreAppRoleName::Holofuel,
+
+        let hash: EntryHashB64 = agent
+            .app
+            .zome_call_typed(
+                CoreAppRoleName::Holofuel.into(),
                 ZomeName::from("transactor"),
                 FunctionName::from("accept_transaction"),
-                ExternIO::encode(AcceptTx {
+                AcceptTx {
                     address: txs.invoice_pending[0].id.clone(),
                     expiration_date: None,
-                })?,
+                },
             )
             .await?;
-        let hash: EntryHashB64 = rmp_serde::from_slice(result.as_bytes())?;
+
         println!("Accepted tx: {:?}", hash);
         println!("Trying to complete, if this fails it will be completed by your schedular");
-        agent
-            .zome_call(
-                CoreAppRoleName::Holofuel,
+
+        let _: EntryHashB64 = agent
+            .app
+            .zome_call_typed(
+                CoreAppRoleName::Holofuel.into(),
                 ZomeName::from("transactor"),
                 FunctionName::from("complete_transactions"),
-                ExternIO::encode(hash)?,
+                hash,
             )
             .await?;
     } else {
