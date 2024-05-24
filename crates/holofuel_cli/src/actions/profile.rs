b7a7010d6c170f629b5094274c210533c90fda46
peeech <pj@imagine-nyc.com>
2024-05-24 13:13:39 +0200

    update holofuel-cli

--- crates/holofuel_cli/src/actions/profile.rs
+++ crates/holofuel_cli/src/actions/profile.rs
@@ -1,19 +1,19 @@
 use anyhow::Result;
-use holochain_types::prelude::{ExternIO, FunctionName, ZomeName};
-use hpos_hc_connect::holofuel_types::Profile;
-use hpos_hc_connect::HolofuelAgent;
+use holochain_types::prelude::{FunctionName, ZomeName};
+use hpos_hc_connect::{app_connection::CoreAppRoleName, hf_agent::HfAgent, holofuel_types::Profile};
 
 pub async fn get() -> Result<()> {
-    let mut agent = HolofuelAgent::connect().await?;
-    let result = agent
-        .zome_call(
+    let mut agent = HfAgent::spawn(None).await?;
+
+    let profile: Profile = agent
+        .app
+        .zome_call_typed(
+            CoreAppRoleName::Holofuel.into(),
             ZomeName::from("profile"),
             FunctionName::from("get_my_profile"),
-            ExternIO::encode(())?,
+            (),
         )
         .await?;
-
-    let profile: Profile = rmp_serde::from_slice(result.as_bytes())?;
 
     println!("===================");
     println!("Your Profile details are: ");
