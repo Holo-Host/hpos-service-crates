d926b303f4297e027715e44e77935c57af8ab66e
JettTech <ldjetton21@gmail.com>
2024-09-10 10:01:19 -0500

    lint

--- crates/core_app_cli/src/actions/get_agents_jurisdiction.rs
+++ crates/core_app_cli/src/actions/get_agents_jurisdiction.rs
@@ -3,15 +3,13 @@ use holochain_types::{
     dna::{AgentPubKey, AgentPubKeyB64},
     prelude::{FunctionName, ZomeName},
 };
-use hpos_hc_connect::{
-    app_connection::CoreAppRoleName, hha_agent::CoreAppAgent, hha_types::PresentedHappBundle,
-};
+use hpos_hc_connect::{app_connection::CoreAppRoleName, hha_agent::CoreAppAgent};
 
 pub async fn get(agent_pubkey: String) -> Result<()> {
     let mut agent = CoreAppAgent::spawn(None).await?;
     let pubkey_bytes: AgentPubKey = AgentPubKeyB64::from_b64_str(&agent_pubkey.clone())?.into();
 
-    let agent_jurisdiction: Vec<(AgentPubKey, Option<String>)> = agent
+    let agent_jurisdictions: Vec<(AgentPubKey, Option<String>)> = agent
         .app
         .zome_call_typed(
             CoreAppRoleName::HHA.into(),
@@ -19,15 +17,11 @@ pub async fn get(agent_pubkey: String) -> Result<()> {
             FunctionName::from("get_agents_jurisdiction"),
             vec![pubkey_bytes],
         )
-        .await?[0];
+        .await?;
 
-    if let Some(d) = agent_jurisdiction.1 {
-        // Please do not change this print and if you do see that the nightly tests that depend on this print are updated as well
+    if let Some(j) = &agent_jurisdictions[0].1 {
         println!("===================");
-        println!(
-            "Hosting Preference for agent {:?}: {}",
-            agent_pubkey, jurisdiction
-        );
+        println!("Jurisdiction for agent {:?}: {:?}", agent_pubkey, j);
         println!("===================");
     } else {
         println!("Error: No jurisdiction found for agent {:?}", agent_pubkey)
